1f681d748c76aca3039dbe717360b6ad1ff238f2
Mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2024-12-20 18:53:40 +0500

    first commit

---
+++ src/app/features/profile/components/mock-user.ts
@@ -0,0 +1,9 @@
+// mock-user.ts
+export const mockUser = {
+  username: "Mukhtarhan",
+  bio: "This is a profile for Mukhtarhan.",
+  image: "https://via.placeholder.com/150",
+  following: false,
+} as Profile;
+
+import { Profile } from "../models/profile.model";
